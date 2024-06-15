958081ccd4e6e04d20470ee496737488e08b40b3
Santhos8 <s92061208@ousl.lk>
2024-06-15 21:14:14 +0530

    changes file will be upload

--- login.php
+++ login.php
@@ -9,7 +9,7 @@
             $password = md5($_POST['password']);
             $user_type = $_POST['user_type'];
 
-            $select = " SELECT * FROM student_signup WHERE name = '$name' && password = '$password' ";
+            $select = " SELECT * FROM student_signup WHERE name = '$email' && password = '$password' ";
 
             $result = mysqli_query($conn, $select);
 
@@ -36,76 +36,26 @@
 <html>
 	<head>
 		<title></title>
-		<link rel="stylesheet" type="text/css" href="css/loginStyle.css">
-        <style>
-            .toggle-btn,.active, .btn:hover {
-                padding:10px 30px;
-                cursor:pointer;
-                background:transparent;
-                border:0;
-                position:relative;
-                
-                
-            }
-
-            .active{
-				background-color:red;
-			}
-            
-        </style>
+		<link rel="stylesheet" type="text/css" href="css/loginStyle.css">         
 	</head>
 	<body>
-	  <div class="main">
-	    <!--create form outer box-->
-		<div class="form-box">
-			<div class="button-box">
-				<div id="btn"></div>
-				<center><button type="button" class="toggle-btn" onclick="login()"><a href="login.php" style="text-decoration:none;">Login</a></button></center>
-				<!-- <button type="button" class="toggle-btn" onclick="signup()"><a href="signupPage.php" style="text-decoration:none;"> Signup</a></button> -->
-			</div>
-			<!--login form-->
-			<form class="input-box" id="login" method="post" action="login.php">
-            <?php 
-                    if(isset($error)){
-                        foreach($error as $error){
-                            echo '<span class="error-msg">' .$error.'</span>';
-                        }
-                    }
-                
-                
-                ?>
-				<input type="text" class="input-field" name="name" placeholder="Username" required>
-				<input type="password" class="input-field" name="password"placeholder="Password" required>
-				<input type="checkbox" class="check-box"><span>Remember Password</span>
-                <input type="submit" name="submit" class="submit-btn" value="Login">
-			</form>
-			
+		<div class="form-box1">
 			<div class="logo">
-				<img src="images/logo.png">
+				<img src="images/logo2.png">
 				<h3>Knowledge Dot</h3>
-			</div>
-			
+			</div>	
+			<h3 style="margin-left: 90px; font-family:Gilroy-Bold; font-weight:400px; margin-top:30px;" >Login</h3>
+			<form  method="post" action="logion.php" class="input-box">
+				<label for="name" class="log-text"> Email</label><br>
+				<input type="text" class="inputValue" name="email" placeholder="username@gmail.com" ><br>
+				<label for="password" class="log-text"> Password</label><br>
+				<input type="password" class="inputValue" name="password" placeholder="Password"><br>
+				<a href = "#" class="link1"><p style="font-size:12px; color:white; margin:5px;margin-left:10px;">Forgot Password?</p></a>
+		        <input type="submit" name="submit" id="submit-btn" value="Login">
+			</form>	
+
+
 		</div>
-	  </div>
-	  
-	  <script>
-		var x = document.getElementById("login");
-		var y = document.getElementById("signup");
-		var z = document.getElementById("btn");
-		
-		function signup(){
-			x.style.left="-400px";
-			y.style.left="50px";
-			z.style.left="110px";
-		}
-		
-		function login(){
-			x.style.left="50px";
-			y.style.left="450px";
-			z.style.left="0px";
-		}
-		
-		
-	  </script>
+	  	
 	</body>
 </html>
